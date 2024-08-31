a8050de50d74ac8b11c74c7069678ab9c8fcd770
Stefan Pejcic <stefan@pejcic.rs>
2024-08-31 20:55:53 +0200

    Update acme-dns-auth.py

--- acme-dns-auth.py
+++ acme-dns-auth.py
@@ -1,154 +1,99 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import json
 import os
-import requests
 import sys
 
-### EDIT THESE: Configuration values ###
-
-# URL to acme-dns instance
-ACMEDNS_URL = "https://auth.acme-dns.io"
-# Path for acme-dns credential storage
-STORAGE_PATH = "/etc/letsencrypt/acmedns.json"
-# Whitelist for address ranges to allow the updates from
-# Example: ALLOW_FROM = ["192.168.10.0/24", "::1/128"]
-ALLOW_FROM = []
-# Force re-registration. Overwrites the already existing acme-dns accounts.
-FORCE_REGISTER = False
-
-###   DO NOT EDIT BELOW THIS POINT   ###
-###         HERE BE DRAGONS          ###
+# Path to the DNS zone files
+ZONE_PATH_TEMPLATE = "/etc/bind/zones/{domain}.zone"
+# Force re-registration. Overwrites the already existing DNS records.
+FORCE_REGISTER = True
 
 DOMAIN = os.environ["CERTBOT_DOMAIN"]
 if DOMAIN.startswith("*."):
     DOMAIN = DOMAIN[2:]
-VALIDATION_DOMAIN = "_acme-challenge."+DOMAIN
+VALIDATION_DOMAIN = "_acme-challenge." + DOMAIN
 VALIDATION_TOKEN = os.environ["CERTBOT_VALIDATION"]
 
-
-class AcmeDnsClient(object):
+class ZoneFileManager(object):
     """
-    Handles the communication with ACME-DNS API
+    Handles the manipulation of the local BIND DNS zone files
     """
 
-    def __init__(self, acmedns_url):
-        self.acmedns_url = acmedns_url
+    def __init__(self, domain):
+        self.zonefile = ZONE_PATH_TEMPLATE.format(domain=domain)
+        self.domain = domain
+        self.load_zone_file()
 
-    def register_account(self, allowfrom):
-        """Registers a new ACME-DNS account"""
-
-        if allowfrom:
-            # Include whitelisted networks to the registration call
-            reg_data = {"allowfrom": allowfrom}
-            res = requests.post(self.acmedns_url+"/register",
-                                data=json.dumps(reg_data))
-        else:
-            res = requests.post(self.acmedns_url+"/register")
-        if res.status_code == 201:
-            # The request was successful
-            return res.json()
-        else:
-            # Encountered an error
-            msg = ("Encountered an error while trying to register a new acme-dns "
-                   "account. HTTP status {}, Response body: {}")
-            print(msg.format(res.status_code, res.text))
+    def load_zone_file(self):
+        """Reads the DNS zone file into memory"""
+        try:
+            with open(self.zonefile, 'r') as fh:
+                self.lines = fh.readlines()
+        except IOError:
+            print(f"ERROR: Could not read zone file {self.zonefile}")
             sys.exit(1)
 
-    def update_txt_record(self, account, txt):
-        """Updates the TXT challenge record to ACME-DNS subdomain."""
-        update = {"subdomain": account['subdomain'], "txt": txt}
-        headers = {"X-Api-User": account['username'],
-                   "X-Api-Key": account['password'],
-                   "Content-Type": "application/json"}
-        res = requests.post(self.acmedns_url+"/update",
-                            headers=headers,
-                            data=json.dumps(update))
-        if res.status_code == 200:
-            # Successful update
-            return
+    def save_zone_file(self):
+        """Writes the DNS zone file back to disk"""
+        try:
+            with open(self.zonefile, 'w') as fh:
+                fh.writelines(self.lines)
+        except IOError:
+            print(f"ERROR: Could not write to zone file {self.zonefile}")
+            sys.exit(1)
+
+    def update_txt_record(self, validation_domain, validation_token):
+        """Updates or adds the TXT record in the zone file"""
+        # Remove any existing TXT record for this validation domain
+        self.lines = [line for line in self.lines if not line.startswith(validation_domain + " ")]
+
+        # Add the new TXT record
+        new_record = f'{validation_domain}. IN TXT "{validation_token}"\n'
+        self.lines.append(new_record)
+
+        # Increment the zone serial number
+        self.increment_serial()
+
+        # Save the updated zone file
+        self.save_zone_file()
+
+        # Reload the BIND server to apply changes
+        self.reload_bind()
+
+    def increment_serial(self):
+        """Increments the serial number in the zone file"""
+        # match the dns zone template for bind9 running on openpanel
+        # https://github.com/stefanpejcic/openpanel-configuration/blob/cdd08aacd287956dc51ce4c34dfdfaf2669a1468/bind9/zone_template.txt#L3
+        for i, line in enumerate(self.lines):
+            if "SOA" in line:
+                serial_idx = i + 1
+                break
         else:
-            msg = ("Encountered an error while trying to update TXT record in "
-                   "acme-dns. \n"
-                   "------- Request headers:\n{}\n"
-                   "------- Request body:\n{}\n"
-                   "------- Response HTTP status: {}\n"
-                   "------- Response body: {}")
-            s_headers = json.dumps(headers, indent=2, sort_keys=True)
-            s_update = json.dumps(update, indent=2, sort_keys=True)
-            s_body = json.dumps(res.json(), indent=2, sort_keys=True)
-            print(msg.format(s_headers, s_update, res.status_code, s_body))
+            print("ERROR: SOA record not found in zone file.")
             sys.exit(1)
 
-class Storage(object):
-    def __init__(self, storagepath):
-        self.storagepath = storagepath
-        self._data = self.load()
+        # Extract the serial number from the identified line
+        serial_line = self.lines[serial_idx].strip()
+        serial_number = int(serial_line.split()[0])
 
-    def load(self):
-        """Reads the storage content from the disk to a dict structure"""
-        data = dict()
-        filedata = ""
-        try:
-            with open(self.storagepath, 'r') as fh:
-                filedata = fh.read()
-        except IOError as e:
-            if os.path.isfile(self.storagepath):
-                # Only error out if file exists, but cannot be read
-                print("ERROR: Storage file exists but cannot be read")
-                sys.exit(1)
-        try:
-            data = json.loads(filedata)
-        except ValueError:
-            if len(filedata) > 0:
-                # Storage file is corrupted
-                print("ERROR: Storage JSON is corrupted")
-                sys.exit(1)
-        return data
+        # Increment the serial number
+        new_serial = serial_number + 1
 
-    def save(self):
-        """Saves the storage content to disk"""
-        serialized = json.dumps(self._data)
-        try:
-            with os.fdopen(os.open(self.storagepath,
-                                   os.O_WRONLY | os.O_CREAT, 0o600), 'w') as fh:
-                fh.truncate()
-                fh.write(serialized)
-        except IOError as e:
-            print("ERROR: Could not write storage file.")
+        # Replace the old serial with the new one, preserving the comment
+        self.lines[serial_idx] = f"                        {new_serial}      ; Serial number\n"
+
+
+    def reload_bind(self):
+        """Reloads the BIND container to apply changes"""
+        ret = os.system("docker exec openpanel_dns rndc reload")
+        if ret != 0:
+            print("ERROR: Failed to reload BIND server.")
             sys.exit(1)
 
-    def put(self, key, value):
-        """Puts the configuration value to storage and sanitize it"""
-        # If wildcard domain, remove the wildcard part as this will use the
-        # same validation record name as the base domain
-        if key.startswith("*."):
-            key = key[2:]
-        self._data[key] = value
-
-    def fetch(self, key):
-        """Gets configuration value from storage"""
-        try:
-            return self._data[key]
-        except KeyError:
-            return None
 
 if __name__ == "__main__":
     # Init
-    client = AcmeDnsClient(ACMEDNS_URL)
-    storage = Storage(STORAGE_PATH)
+    zone_manager = ZoneFileManager(DOMAIN)
 
-    # Check if an account already exists in storage
-    account = storage.fetch(DOMAIN)
-    if FORCE_REGISTER or not account:
-        # Create and save the new account
-        account = client.register_account(ALLOW_FROM)
-        storage.put(DOMAIN, account)
-        storage.save()
-
-        # Display the notification for the user to update the main zone
-        msg = "Please add the following CNAME record to your main DNS zone:\n{}"
-        cname = "{} CNAME {}.".format(VALIDATION_DOMAIN, account["fulldomain"])
-        print(msg.format(cname))
-
-    # Update the TXT record in acme-dns instance
-    client.update_txt_record(account, VALIDATION_TOKEN)
+    # Update the TXT record in the local zone file
+    zone_manager.update_txt_record(VALIDATION_DOMAIN, VALIDATION_TOKEN)
